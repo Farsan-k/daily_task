86ad1e4b38900dd7c799b13b090ecaa3ea86c16a
Farsan <farsanfarsank41@gmail.com>
2026-07-10 17:32:02 +0530

    Some change in CNN

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 import numpy as np
 
-
-stride = 1
+stried = 1
 
 image = np.array([
-    [1,2,3,1,0],
-    [0,1,2,3,1],
-    [1,2,0,3,0],
-    [1,2,0,3,0],
-    [1,0,3,2,1]
+    [1,2,3,0,1],
+    [1,2,2,0,1],
+    [2,1,3,1,2],
+    [3,2,1,1,0],
+    [0,2,1,3,1]
 ])
 
-kernel =  np.array([
+kernel = np.array([
     [1,0,1],
     [0,1,0],
-    [1,0,1]
+    [1,0,1],
 ])
 
 image_h, image_w = image.shape
 
 k = kernel.shape[0]
 
-output_h = (image_h - k) + 1 // stride
-output_w = (image_w - k) + 1 // stride
-
+output_h = ((image_h - k) // stried) + 1
+output_w = ((image_w - k) // stried) + 1
 
 output = np.zeros(
     [output_h,
@@ -31,12 +29,12 @@ output = np.zeros(
 )
 
 for i in range(output_h):
-  for j in range(output_w):
-    region = image[
-        i * stride : i * stride + k,
-        j * stride : j * stride + k
-    ]
+    for j in range(output_w):
+        region = image[
+            i * stried : i * stried + k,
+            j * stried : j * stried + k
+        ]
 
-    output[i,j] = np.sum(region * kernel)
+        output[i,j] = np.sum(region * kernel)
 
 print(output)
