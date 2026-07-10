a125cb8a60e84e4e7a820fa2442639d4ee28367c
Farsan <farsanfarsank41@gmail.com>
2026-07-10 16:33:24 +0530

    CNN MODEL

--- main.py
+++ main.py
@@ -1,2 +1,42 @@
 import numpy as np
-import pandas as pd
+
+
+stride = 1
+
+image = np.array([
+    [1,2,3,1,0],
+    [0,1,2,3,1],
+    [1,2,0,3,0],
+    [1,2,0,3,0],
+    [1,0,3,2,1]
+])
+
+kernel =  np.array([
+    [1,0,1],
+    [0,1,0],
+    [1,0,1]
+])
+
+image_h, image_w = image.shape
+
+k = kernel.shape[0]
+
+output_h = (image_h - k) + 1 // stride
+output_w = (image_w - k) + 1 // stride
+
+
+output = np.zeros(
+    [output_h,
+     output_w]
+)
+
+for i in range(output_h):
+  for j in range(output_w):
+    region = image[
+        i * stride : i * stride + k,
+        j * stride : j * stride + k
+    ]
+
+    output[i,j] = np.sum(region * kernel)
+
+print(output)
